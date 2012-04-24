b4904a6294e5cb124356d9ceb485c994ffabf058
student20@example.com <student20@example.com>
2012-04-24 17:29:09 +0000

    update file /client/default/app/views/Viewport.js

--- client/default/app/views/Viewport.js
+++ client/default/app/views/Viewport.js
@@ -27,7 +27,7 @@ app.views.Viewport = Ext.extend(Ext.Panel, {
     // Put instances of cards (views) into viewport here
     Ext.apply(this, {
       items: [
-          app.views.Home
+          new app.views.Home
       ]
     });
     app.views.Viewport.superclass.initComponent.apply(this, arguments);
