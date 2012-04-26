4ef83d63cf37241ba942623c9b59b3d2f57f1bba
patrick.oneill46@gmail.com <patrick.oneill46@gmail.com>
2012-04-26 13:35:55 +0000

    update file /client/default/app/views/Gauge.js

--- client/default/app/views/Gauge.js
+++ client/default/app/views/Gauge.js
@@ -1,48 +1,25 @@
-app.views.Gauge = Ext.extend(Ext.Panel, {
+app.views.Gauge = Ext.extend( Ext.Panel, {
+  id: "gauge",
+  current_angle: 0,
+  height: 150,
+  layout: {
+    type: "vbox",
+    align: "stretch"
+  },
 
-  title: 'Gauge',
-  iconCls: 'home',
-  scroll: 'vertical',
-  height: '100%',
-  layout: 'fit',
-  
-  dockedItems: [
-    {
-    	dock: 'top',
-  		xtype: 'toolbar',
-      title: '<img class="logo" src="app/images/indo/top_background_full.png" />',
-  		items: [
-  			{
-  				text: 'Back',
-          ui: 'back',
-          hidden: app.hideBack || false,
-  				handler: function() {
-  					app.views.viewport.setActiveItem(app.views.home, {type: 'slide', direction: 'right'});
-            app.stores.stocks.removeAll();
-  				}
-  			}
-  		]
-  	}
-  ],
-  
   items: [{
-    xtype: 'panel',
-    layout:{
-      type:'vbox',
-      align: 'center'
-    },
-    //defaults:{flex:'1'},
-    items: [{xtype:'panel', html:'How loyal a reader are you?'},{
-          xtype: 'button',
-          text: 'Submit',
-          handler: function() {
-            //Ext.dispatch({
-              //controller: app.controllers.barcode,
-              //action: 'scanCode'
-            //});
-          }
-        }
-    ],
-   }]
+    cls: "minute_monitor",
+    html: '<div id="needle"></div>'
+  }],
 
+  rotateTo: function(deg) {
+    var needle = Ext.get('needle').dom;
+  console.log(needle);
+    Wilq32.PhotoEffect._handleRotation(needle, {
+      angle: this.current_angle,
+      animateTo: deg
+    });
+    this.current_angle = deg;
+  },
+  
 });
