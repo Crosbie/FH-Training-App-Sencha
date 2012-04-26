1d7ad00feeac239189f22b34c2181e9d606c4a2d
patrick.oneill46@gmail.com <patrick.oneill46@gmail.com>
2012-04-26 09:15:45 +0000

    update file /client/default/app/views/Barcode.js

--- client/default/app/views/Barcode.js
+++ client/default/app/views/Barcode.js
@@ -5,4 +5,23 @@ app.views.Barcode = Ext.extend(Ext.Panel, {
   scroll: 'vertical',
   height: '100%',
   layout: 'fit',
+  
+  dockedItems: [
+    {
+  		dock: 'top',
+  		xtype: 'toolbar',
+      title: '<img class="logo" src="app/images/indo/top_background_full.png" />',
+  		items: [
+  			{
+  				text: 'Back',
+          ui: 'back',
+          hidden: app.hideBack || false,
+  				handler: function() {
+  					app.views.viewport.setActiveItem(app.views.home, {type: 'slide', direction: 'right'});
+            app.stores.stocks.removeAll();
+  				}
+  			}
+  		]
+  	}
+  ],
 });
