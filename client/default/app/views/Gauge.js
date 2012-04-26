5edf14698cf25ff1dfd71e12de4be29f95efeefb
patrick.oneill46@gmail.com <patrick.oneill46@gmail.com>
2012-04-26 09:50:22 +0000

    update file /client/default/app/views/Gauge.js

--- client/default/app/views/Gauge.js
+++ client/default/app/views/Gauge.js
@@ -1,4 +1,48 @@
 app.views.Gauge = Ext.extend(Ext.Panel, {
 
+  title: 'Gauge',
+  iconCls: 'home',
+  scroll: 'vertical',
+  height: '100%',
+  layout: 'fit',
+  
+  dockedItems: [
+    {
+    	dock: 'top',
+  		xtype: 'toolbar',
+      title: '<img class="logo" src="app/images/indo/top_background_full.png" />',
+  		items: [
+  			{
+  				text: 'Back',
+          ui: 'back',
+          hidden: app.hideBack || false,
+  				handler: function() {
+  					app.views.viewport.setActiveItem(app.views.home, {type: 'slide', direction: 'right'});
+            app.stores.stocks.removeAll();
+  				}
+  			}
+  		]
+  	}
+  ],
+  
+  items: [{
+    xtype: 'panel',
+    layout:{
+      type:'vbox',
+      align: 'center'
+    },
+    //defaults:{flex:'1'},
+    items: [{
+          xtype: 'button',
+          text: 'Submit',
+          handler: function() {
+            //Ext.dispatch({
+              //controller: app.controllers.barcode,
+              //action: 'scanCode'
+            //});
+          }
+        }
+    ],
+   }]
 
-})
+});
