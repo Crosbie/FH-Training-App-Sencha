5f0191fd7d8d6ed44e0b1785402355f0984593f4
patrick.oneill@bearingpoint.com <patrick.oneill@bearingpoint.com>
2012-05-03 10:17:00 +0000

    update file /client/default/app/views/Settings.js

--- client/default/app/views/Settings.js
+++ client/default/app/views/Settings.js
@@ -17,7 +17,7 @@ app.views.Settings = Ext.extend(Ext.Panel, {
   	{
   		dock: 'top',
   		xtype: 'toolbar',
-      title: '<img class="logo" src="app/images/rewards_black.png" />',
+      title: '<img class="logo" src="app/images/indo/rewards_black.png" />',
       height: 60,
   		items: [
   			{
